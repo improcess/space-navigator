80ed3fa5eac3e5084d6a3362016058f23b7dc236
Sam Aaron <samaaron@gmail.com>
2010-12-27 17:11:32 +0000

    update native-deps to 1.0.5 for lein 1.4.x compatibility

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A library with which to read values from a 3DConnexion SpaceNavigator 3D mouse"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[native-deps "1.0.4"]]
+  :dev-dependencies [[native-deps "1.0.5"]]
   :native-dependencies [[overtone/jinput "1.0.0"]])
